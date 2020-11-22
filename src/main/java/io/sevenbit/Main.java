b82f5c059637c6a565f5430ab652833d96a1e5f6
Andrey Belyakov <andrey.belyakov@lifestreet.com>
2020-11-23 00:02:55 +0300

    Example rewritten with java-8 style

--- src/main/java/io/sevenbit/Main.java
+++ src/main/java/io/sevenbit/Main.java
@@ -8,22 +8,26 @@ import java.nio.ByteBuffer;
 
 public class Main {
 
+
     public static void main(String[] args) throws InterruptedException {
-        AppEventFactory eventFactory = new AppEventFactory();
         int bufferSize = 1024;
-        Disruptor<AppEvent> disruptor = new Disruptor<>(eventFactory, bufferSize, DaemonThreadFactory.INSTANCE);
-        disruptor.handleEventsWith(new AppEventHandler());
+        Disruptor<AppEvent> disruptor = new Disruptor<>(AppEvent::new, bufferSize, DaemonThreadFactory.INSTANCE);
+        disruptor.handleEventsWith(Main::handleEvent);
         disruptor.start();
-
         RingBuffer<AppEvent> ringBuffer = disruptor.getRingBuffer();
-        AppEventProducer producer = new AppEventProducer(ringBuffer);
-
         ByteBuffer bb = ByteBuffer.allocate(8);
-
         for (long l = 0; true; l++) {
             bb.putLong(0, l);
-            producer.onData(bb);
+            ringBuffer.publishEvent(Main::translate, bb);
             Thread.sleep(1000);
         }
+    }
+
+    public static void handleEvent(AppEvent event, long sequence, boolean endOfBatch) {
+        System.out.println(event);
+    }
+
+    public static void translate(AppEvent event, long sequence, ByteBuffer buffer) {
+        event.setValue(Long.toString(buffer.getLong(0)));
     }
 }
